6181babf16427967493e3984378231314471d1fe
drjif <159730411+drjif@users.noreply.github.com>
2026-08-15 05:07:16 -0400

    Initialize local file import UI before event binding

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import {
   updateControls
 } from './dom.js';
 import { setupBetaFunnel } from './beta.js';
+import { setupFileImportUI } from './file-import-ui.js';
 import { setupHomepageMarketing } from './homepage-marketing.js';
 import { handleWakeLockPreferenceChange, releaseSessionWakeLock, setupPowerManagement } from './power.js';
 import { setupSheetEvents } from './sheet-v2.js';
@@ -75,6 +76,7 @@ function setupInstallation() {
 
 function initialize() {
   setupHomepageMarketing();
+  setupFileImportUI();
   restoreSettings();
   setupBetaFunnel();
   setupSheetEvents();
