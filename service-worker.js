618839724e7b7ce65b0f056012458a2f36756e74
drjif <159730411+drjif@users.noreply.github.com>
2026-09-13 07:23:26 -0700

    Cache account presentation update

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'same3le-v18-study-workspace';
+const CACHE_NAME = 'same3le-v19-account-presentation';
 const ASSETS = [
   './',
   './index.html',
@@ -11,6 +11,7 @@ const ASSETS = [
   './starred-questions.js',
   './stars-ui.js',
   './star-button-style.js',
+  './account-presentation.js',
   './source-focus.js',
   './homepage-marketing.js',
   './file-import-ui.js',
